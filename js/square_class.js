e5364a0309bf5e44a1a681b5d9386d02fc34fa89
Artem Afonchenkov <56221032+artafan1106@users.noreply.github.com>
2021-11-25 19:25:30 +0300

    Update square_class.js
    Небольшие изменения для генерации квадратов

--- js/square_class.js
+++ js/square_class.js
@@ -1,18 +1,17 @@
 class Square {
-    constructor(id, ctx) {
-        this.ctx = ctx;
+    constructor(id) {
+        this.ctx = canvas.getContext('2d');
         this.id = id;
-        this.width = 1 + (Math.random() % 100 + 50);
-        this.height = 1 + (Math.random() % 100 + 50);
+        this.width = 60;
+        this.height = 60;
+        this.ctx.x = 10;
+        this.ctx.y = 10;
         this.color = "#" + Math.round(Math.random() * 16777215).toString(16);
     }
-    getId() {
-        return this.id;
-    }
     draw() {
-        this.ctx.save(); // ругается здесь хз почему (this.ctx is undefined)
+        this.ctx.save();
         this.ctx.fillStyle = this.color;
-        this.ctx.fillRect((this.width / 2),(this.height / 2),150,150);
+        this.ctx.fillRect(this.ctx.x,this.ctx.y, this.width, this.height);
         this.ctx.restore();
     }
 }
