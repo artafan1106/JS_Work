dac8069491b09365facec32b9efcc62adb7725ef
Artem Afonchenkov <56221032+artafan1106@users.noreply.github.com>
2021-12-02 16:38:44 +0300

    Работа с анимацией + фиксы +доработки. (#3)
    * ';'
    
    * this.ctx is undefined
    
    * Create text.txt
    
    * Rename object.js to js/object.js
    
    * Rename square_class.js to js/square_class.js
    
    * Rename app.js to js/app.js
    
    * Add files via upload
    
    * Rename index.css to css/index.css
    
    * Add files via upload
    
    * Delete text.txt
    
    * Rename object.js to app-class.js
    
    * Rename app-class.js to app_class.js
    
    * Delete app.js
    
    * Update index.html
    
    * Update index.css
    
    * Add files via upload
    
    * Update index.html
    
    Удалил лишнее
    
    * Update app_class.js
    
    Удаление лишнего + доработки по генерации квадратов.
    
    * Update square_class.js
    
    Небольшие изменения для генерации квадратов
    
    * Update index.css
    
    Удаление лишнего
    
    * Update app_class.js
    
    * Delete app.js
    
    * Rename app_class.js to app.js
    
    * Update index.html
    
    * Add files via upload
    
    * Create text.txt
    
    * Delete doge.jpg
    
    * Add files via upload
    
    * Исправления в app.js
    
    1. Подкорректировано смещение квадратов.
    2. Увеличено кол-во генерации квадратов с 10 до 25.
    3. Исправил имя массива.
    
    * app.js добавил счетчик FPS.
    
    Добавил счетчик FPS (отрисовка в canvas с обновлением).
    
    * Исправления +   добавление анимации.
    
    * Доработка кода + нейминг.
    
    * Update app.js
    
    * Update and rename square_class.js to tile_class.js

---
+++ js/app.js
@@ -0,0 +1,47 @@
+import Square from './tile_class.js';
+
+
+class Application {
+    constructor() {
+        this.canvas = document.getElementById('canvas');
+        this.ctx = canvas.getContext('2d');
+        this.canvas.width = 600;
+        this.canvas.height = 600;
+        this.tiles = [];
+    }
+
+    generateTiles() {
+        for (let tileCounter = 0; tileCounter < 25; tileCounter++) {
+            let tile = new Tile(tileCounter, this.ctx);
+            this.tiles.push(tile);
+        }
+    }
+    run() {
+        this.generateTiles();
+        let counter = 1;
+        for (let i = 0; i < 25; i++) {
+            let tile = this.tiles[i];
+            tile.draw();
+            if (counter % 5 != 0) {
+                this.ctx.y += 80;
+            } else {
+                this.ctx.x += 150;
+                this.ctx.y -= 320;
+            }
+            counter += 1;
+        }
+    }
+}
+const app = new Application();
+
+requestAnimationFrame(tick);
+
+function tick(timestamp) {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    requestAnimationFrame(tick);
+    const diff = timestamp - pTimestamp;
+    pTimestamp = timestamp;
+    ctx.clearRect(0,0,800,800);
+    app.run();
+}
