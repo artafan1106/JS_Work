265452074c819fe9874c616a8e951034695d1bb8
Artem Afonchenkov <56221032+artafan1106@users.noreply.github.com>
2021-12-02 16:11:42 +0300

    Доработка кода + нейминг.

--- js/square_class.js
+++ js/square_class.js
@@ -1,18 +1,19 @@
-class Square {
-    constructor(id) {
-        this.ctx = canvas.getContext('2d');
+class Tile {
+    constructor(id, ctx) {
+        this.ctx = ctx;
         this.id = id;
-        this.width = 60;
-        this.height = 60;
-        this.ctx.x = 10;
-        this.ctx.y = 10;
+        this.width = 90;
+        this.height = 50; // Math.round(Math.random() * 100)
+        this.ctx.x = 50;
+        this.ctx.y = 50;
         this.color = "#" + Math.round(Math.random() * 16777215).toString(16);
     }
     draw() {
-        this.ctx.save();
         this.ctx.fillStyle = this.color;
-        this.ctx.fillRect(this.ctx.x,this.ctx.y, this.width, this.height);
-        this.ctx.restore();
+        this.ctx.fillRect(this.ctx.x, this.ctx.y, this.width, this.height);
+        this.ctx.translate(this.ctx.x + this.width / 2,this.ctx.y + this.height / 2);
+        this.ctx.rotate(0.0003);
+        this.ctx.translate(-(this.ctx.x + this.width / 2),-(this.ctx.y + this.height / 2));
     }
 }
-export default Square;
+export default Tile;
