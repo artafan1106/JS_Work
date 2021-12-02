cae174e76eb6ae9cc51a133331c2106e2a83d79e
Artem Afonchenkov <56221032+artafan1106@users.noreply.github.com>
2021-12-02 16:10:03 +0300

    Исправления +   добавление анимации.

--- js/app.js
+++ js/app.js
@@ -10,49 +10,43 @@ class Application {
         this.lst_objects = [];
     }
 
-    generateSquares() {
-        for (let squareCnt = 1; squareCnt < 26; squareCnt++) {
-            let square = new Square(squareCnt);
-            let square_draw = new Square(this.ctx);
-            this.lst_objects.push(square);
-            square_draw.draw();
-            if (squareCnt % 5 == 0) {
-                this.ctx.translate(-360,70);
+    generateTiles() {
+        for (let tileCounter = 0; tileCounter < 25; tileCounter++) {
+            let tile = new Tile(tileCounter, this.ctx);
+            this.tiles.push(tile);
+        }
+    }
+    run() {
+        this.generateTiles();
+        let counter = 1;
+        for (let i = 0; i < 25; i++) {
+            let tile = this.tiles[i];
+            tile.draw();
+            if (counter % 5 != 0) {
+                this.ctx.y += 80;
             } else {
-                this.ctx.translate(70, 0);
+                this.ctx.x += 150;
+                this.ctx.y -= 320;
             }
+            counter += 1;
         }
     }
 }
 const app = new Application();
-app.generateSquares();
-
-let frameCount = function _fc(timeStart){
-    var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
-    let now = performance.now();
-    let duration = now - timeStart;
-
-    if(duration < 1000){
-        _fc.counter++;
-    } else {
-
-        _fc.fps = _fc.counter;
-        _fc.counter = 0;
-        timeStart = now;
-        console.log(_fc.fps);
-        ctx.clearRect(0, 0, 800, 800);
-        ctx.font = "42px Arial";
-        if (_fc.fps < 60) {
-            ctx.fillStyle = 'red';
-        } else {
-            ctx.fillStyle = 'green';
-        }
-        ctx.fillText('FPS: ' + _fc.fps,630, 330);
-    }
-    requestAnimationFrame(() => frameCount(timeStart));
-}
 
 frameCount.counter = 0;
 frameCount.fps = 0;
 frameCount(performance.now());
+
+requestAnimationFrame(tick);
+
+function tick(timestamp) {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    requestAnimationFrame(tick);
+    const diff = timestamp - pTimestamp;
+    pTimestamp = timestamp;
+    ctx.clearRect(0,0,800,800);
+    ctx.beginPath();
+    app.run();
+}
